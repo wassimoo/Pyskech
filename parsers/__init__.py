9b90ef43c5de5ee68ce87cdad7c74eb427559cb3
wassim@nuevera.com <wassimjbaali5121997@gmail.com>
2018-10-19 18:42:41 +0100

    licence header updated

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,5 +1,5 @@
 #
-# Peskech - Automated data wrangling for data scientists
+# Peskech - Automated data structuring for data scientists
 # Copyright (C) 2018 Wassim Bougarfa
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 __all__ = [
